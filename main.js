70a4a1a27ac1b951713a67e32f55fa06e216a997
vagrant <vagrant@LyricOps-berkshelf.(none)>
2013-07-06 09:00:42 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+(function($){
+ 
+  var ListView = Backbone.View.extend({
+    el: $('body'),
+ 
+    initialize: function(){
+      _.bindAll(this, 'render');
+      this.render();
+    },
+ 
+    render: function(){
+      $(this.el).append("<ul><li>hello world</li></ul>");
+    }
+  });
+ 
+  var listView = new ListView();
+})(jQuery);
