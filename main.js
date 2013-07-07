ba9f849423eae74564674f2758f596cca559433d
vagrant <vagrant@LyricOps-berkshelf.(none)>
2013-07-07 06:47:52 +0000

    added

--- main.js
+++ main.js
@@ -9,9 +9,43 @@
     },
  
     render: function(){
+		var www = new Host({name: 'www'});
+		var lyric = new Host({name: 'www', domain: 'lmail.jp'});
+		
       $(this.el).append("<ul><li>hello world</li></ul>");
+      $(this.el).append(www.getFQDN());
+      $(this.el).append(lyric.getFQDN());
+
+		var xssmodel = new XssModel();
+      $(this.el).append(xssmodel.escape("attr"));
+      $(this.el).append(xssmodel.getXssString());
+
+		var model = new Backbone.Model();
+
+		model.on('change', function(model){
+			_.each(model.changedAttributes(), function(value, name){
+				console.log(name + ":" + model.previous(name) + " -> " + value);
+			});
+		});
+
+		model.set('foo', 1);
+		model.set('foo', 3);
+
+		var he = new Person();
+		var pre = $(this.el);
+		he.on('invalid', function(msg){
+			console.log(msg.validationError);
+			console.log($("#errorout").text(msg.validationError));
+		});
+
+		he.save();
+
+		he.set('age', 1000, {validate: true});
+
     }
   });
- 
+
   var listView = new ListView();
+
 })(jQuery);
+
